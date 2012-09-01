88e6898e4f668610c27f8aeed60ca93f010cb289
Xevi Subirà <xavier_subira@yahoo.es>
2012-09-01 17:53:58 +0200

    Sh containing the shell script lines to recreate ER diagrams using schemaSpy

---
+++ db/recreate_diagrams.sh
@@ -0,0 +1,2 @@
+# RECREA LOS DIAGRAMAS DE LA BASE DE DATOS cooxino_common
+java -jar schemaSpy_5.0.0.jar -t mysql -db cooxino_common -u cooxino -host stevie.heliohost.org -port 3306 -p susoITZ4 -dp /usr/share/java/mysql.jar -o ./diagrams/common/
